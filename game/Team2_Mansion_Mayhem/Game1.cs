547e5a76057f09108a31f214cbead8f82c21f8cb
jd6030 <jd6030@rit.edu>
2024-03-06 10:20:28 -0500

    Early structure of the GameStates

--- game/Team2_Mansion_Mayhem/Game1.cs
+++ game/Team2_Mansion_Mayhem/Game1.cs
@@ -4,16 +4,29 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Team2_Mansion_Mayhem
 {
+    enum GameState
+    {
+        MainMenu,
+        Game,
+        GameOver
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private GameState currentState;
+        private KeyboardState kbState;
+        private KeyboardState preKbState;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            currentState = GameState.MainMenu;
+            kbState = new KeyboardState();
         }
 
         protected override void Initialize()
@@ -35,7 +48,30 @@ namespace Team2_Mansion_Mayhem
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            kbState = Keyboard.GetState();
+            switch (currentState)
+            {
+                case GameState.MainMenu:
+                    // starts game by pressing space
+                    if(preKbState.IsKeyUp(Keys.Space) && kbState.IsKeyDown(Keys.Space))
+                    {
+                        currentState = GameState.Game;
+                    }
+                    break;
+                case GameState.Game:
+                    // in game update logic
+                    break;
+                case GameState.GameOver:
+                    // sends player back to main menu when they press Space on the GameOver screen
+                    if (preKbState.IsKeyUp(Keys.Space) && kbState.IsKeyDown(Keys.Space))
+                    {
+                        currentState = GameState.MainMenu;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            preKbState = kbState;
 
             base.Update(gameTime);
         }
